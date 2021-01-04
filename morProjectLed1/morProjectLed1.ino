46257b453cf15462f4ce6493b65dc8595c05fcec
Asaf <asafzenou97@gmail.com>
2021-01-04 20:41:20 +0200

    Adding function turn off from start to end

--- morProjectLed1/morProjectLed1.ino
+++ morProjectLed1/morProjectLed1.ino
@@ -1,5 +1,5 @@
-int listSize = 8;
-int ledList[8]= {13, 12, 11, 10, 9, 8, 7, 6};
+const int listSize = 8;
+int ledList[listSize]= {13, 12, 11, 10, 9, 8, 7, 6};
 
 void setup() {
   Serial.begin(9600);
@@ -9,14 +9,20 @@ void setup() {
   }  
 }
 
+void off_FromTheEndToStart();
+
 void loop() {
   turningOnA_Heart_M();
-  off_FromTheEndToStart(); 
+  off_FromTheStartToEnd(); 
+
+  turningOnA_Heart_M();
+  off_FromTheEndToStart();
 }
 
 
 // This function will turn A and then Heart and then the M in that order
 void turningOnA_Heart_M(){
+   for(int i=0; i < 4; i++){
     digitalWrite(ledList[i], HIGH);
     delay(700);
   }
@@ -29,9 +35,19 @@ delay(3000);
 }
 
 
-// Turning off all leds from the end to that start
+// Turning off all leds from the end to start
 void off_FromTheEndToStart(){
   for(int i=listSize-1; i >= 0; i--){
+    digitalWrite(ledList[i], LOW);
+    delay(700);
+    Serial.println("The i is " + String(i) );
+    Serial.println("The number is " +String(ledList[i]) );    
+  }  
+}
+
+// Turning off all leds from the start to end
+void off_FromTheStartToEnd(){
+  for(int i=0; i < listSize ; i++){
     digitalWrite(ledList[i], LOW);
     delay(700);
     Serial.println("The i is " + String(i) );

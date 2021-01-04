5fe8ad815e0b60c2d363256d2353b9d86c1a7b65
Asaf <asafzenou97@gmail.com>
2021-01-04 21:59:05 +0200

    All the function without the ultra sonic

--- morProjectLed1/morProjectLed1.ino
+++ morProjectLed1/morProjectLed1.ino
@@ -1,11 +1,13 @@
-const int listSize = 8;
-int ledList[listSize]= {13, 12, 11, 10, 9, 8, 7, 6};
+const int listSize = 9;
+int ledList[listSize]= {13, 12, 11, 10, 9, 8, 7, 6, 5};
 
 void setup() {
   Serial.begin(9600);
   delay(1000);
+  pinMode(4, OUTPUT);
   for(int i=0; i < listSize; i++){
     pinMode(ledList[i], OUTPUT);
+    
   }  
 }
 
@@ -13,25 +15,26 @@ void off_FromTheEndToStart();
 
 void loop() {
   turningOnA_Heart_M();
-  off_FromTheStartToEnd(); 
-
-  turningOnA_Heart_M();
-  off_FromTheEndToStart();
+  
 }
 
 
 // This function will turn A and then Heart and then the M in that order
 void turningOnA_Heart_M(){
-   for(int i=0; i < 4; i++){
+  off_All_Leds_Together();
+   for(int i=0; i < listSize/3; i++){
     digitalWrite(ledList[i], HIGH);
-    delay(700);
   }
-delay(3000);
+  delay(3000);
     
-  for(int i=4; i < listSize; i++){
+  for(int i=listSize/3; i < (2*listSize)/3; i++){
     digitalWrite(ledList[i], HIGH);
-    delay(700);
   }
+  delay(3000);
+  for(int i=(2*listSize)/3; i < listSize; i++){
+    digitalWrite(ledList[i], HIGH);
+  }
+  delay(5000);
 }
 
 
@@ -53,4 +56,34 @@ void off_FromTheStartToEnd(){
     Serial.println("The i is " + String(i) );
     Serial.println("The number is " +String(ledList[i]) );    
   }  
+}
+
+
+// Turning on all the leds in the same time
+void on_All_Leds_Together(){
+  for(int i=0; i < listSize; i++){
+    digitalWrite(ledList[i], HIGH);
+  }
+}
+
+
+// Turning on all the leds in the same time
+void off_All_Leds_Together(){
+  for(int i=0; i < listSize; i++){
+    digitalWrite(ledList[i], LOW);
+  }
+}
+
+// Turning on all even leds together
+void on_All_even_Leds_Together(){
+  for(int i=0; i < listSize; i+=2){
+    digitalWrite(ledList[i], HIGH);
+  }
+}
+
+// Turning on all not even leds together
+void on_All_not_even_Leds_Together(){
+  for(int i=1; i < listSize; i+=2){
+    digitalWrite(ledList[i], HIGH);
+  }
 }
